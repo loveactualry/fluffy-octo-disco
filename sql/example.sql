44a7b0819755fd7c0cba8ab7b7d57fc906e22c12
loveactualry <loveactualry@gmail.com>
2020-05-03 00:56:35 +0900

    right,left update

--- sql/example.sql
+++ sql/example.sql
@@ -34,3 +34,10 @@ SELECT DATE_FORMAT(DATETIME, '%Y-%m-%d %h:%i:%s') FROM ANIMAL_OUTS;
 REGULAR EXPRESSION
 https://kutar37.tistory.com/entry/oracle-REGEXPLIKE
 
+RIGHT, LEFT
+https://www.hackerrank.com/challenges/more-than-75-marks/forum
+https://pangate.com/675
+
+오른쪽 왼쪽 처리에 대해 같은 결과지만 다른 방법으로 풀어내고 있음
+
+
